088b0b45a334d56a8d6c0cf8d73c85a4a85d0a67
UltronOne <41232566+UltronOne@users.noreply.github.com>
2020-11-17 19:56:33 +0100

    Added tailwind css

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,53 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 import React, { Component } from 'react';
-import { render } from 'react-dom';
+import {Line} from 'react-chartjs-2';
 
-
-
+const data = {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  datasets: [
+    {
+      label: 'My First dataset',
+      fill: false,
+      lineTension: 0.1,
+      backgroundColor: 'rgba(75,192,192,0.4)',
+      borderColor: 'rgba(75,192,192,1)',
+      borderCapStyle: 'butt',
+      borderDash: [],
+      borderDashOffset: 0.0,
+      borderJoinStyle: 'miter',
+      pointBorderColor: 'rgba(75,192,192,1)',
+      pointBackgroundColor: '#fff',
+      pointBorderWidth: 1,
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+      pointHoverBorderColor: 'rgba(220,220,220,1)',
+      pointHoverBorderWidth: 2,
+      pointRadius: 1,
+      pointHitRadius: 10,
+      data: [65, 59, 80, 81, 56, 55, 40]
+    }
+  ]
+};
 
 
 export default function Home() {
 
   
 
+
   
   return (
    <>
 
-<canvas  id="canvasDemo" height="400" width="300">
-Sorry, your browser does not support canvas.
-</canvas>
-
+  
+<div className="p-64">
+    <h2>Line Example</h2>
+    <Line
+      data={data}
+      width={400}
+      height={400}
+    />
+  </div>
+  
 
   </>
   )
